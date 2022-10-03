463ec369b7993164f27e90d1a55353d7ba55179c
lhh1171 <2807652103@qq.com>
2022-10-03 22:26:11 +0800

    testRegionServer

--- vcdb-RegionServer/src/test/java/org/example/vcdb/store/region/TestRegionServer.java
+++ vcdb-RegionServer/src/test/java/org/example/vcdb/store/region/TestRegionServer.java
@@ -51,12 +51,13 @@ public class TestRegionServer {
         serverMeta.dis();
     }
     @Test
-    public void testAll(){
+    public void testCreateDB(){
         String dbName="db2";
         String tabName="table2";
         String regionMetaFileName="region/regionMeta22";
         String fileStoreName="fileStore/fileStore2";
         /*createTable*/
+        /*注册该表到RegionServerMeta*/
         /*先读出RegionServerMeta*/
         RegionServerMeta serverMeta = new RegionServerMeta(VCFileReader.readAll("regionServerMeta"));
         Map<String, String> regionMap = serverMeta.getRegionMap();
@@ -66,44 +67,23 @@ public class TestRegionServer {
         serverMeta.setRegionMap(regionMap);
         /*写入文件*/
         VCFIleWriter.writerAll(serverMeta.getData(), "regionServerMeta");
-//        List<ColumnFamilyMeta> column_family=new ArrayList<>();
-//        for (int i = 0; i < 3; i++) {
-//            column_family.add(new ColumnFamilyMeta(true, false, i, 100, ColumnFamilyMeta.byteToCFType((byte) 44))) ;
-//        }
 
-        /*写regionMeta*/
+        /*创建regionMeta*/
         Map<String, String> fileStoreMap = new ConcurrentHashMap<>();
         for (int i = 1; i < 4; i++) {
             regionMap.put("cf" + i, "fileStoreMeta/fileStoreMeta" + i);
         }
         RegionMeta regionMeta = new RegionMeta(regionMetaFileName, fileStoreMap);
         VCFIleWriter.writerAll(regionMeta.getData(), regionMetaFileName);
-        /*写 && fileStoreMeta*/
 
-        List<KVRange> list=new ArrayList<>();
-        for (int i = 1; i < 4; i++) {
-            list.add(new KVRange(1111,"startKey"+i,"endKey"+i));
-        }
+        /*创建fileStoreMeta*/
         FileStoreMeta fileStoreMeta = new FileStoreMeta((new Date()).getTime(), false,
-                fileStoreName, "r1".getBytes(), "r2".getBytes(), tabName,dbName,list);
+                fileStoreName, "r1".getBytes(), "r2".getBytes(), tabName,dbName);
         VCFIleWriter.writerAll(fileStoreMeta.getData(), fileStoreName);
 
-        /*写fileStore*/
+        /*创建fileStore*/
         ColumnFamilyMeta cfMeta = new ColumnFamilyMeta(true, false, 1, 100, ColumnFamilyMeta.byteToCFType((byte) 44));
-        byte[] row = "row1".getBytes(StandardCharsets.UTF_8);
-        byte[] family = "fam1".getBytes(StandardCharsets.UTF_8);
-        List<KV.ValueNode> values = new ArrayList<>();
-        for (int i = 1; i <= 2; i++) {
-            long time = (new Date()).getTime();
-            KV.Type type = KV.byteToType((byte) 4);
-            byte[] qualifier = ("qualifier" + i).getBytes(StandardCharsets.UTF_8);
-            byte[] value = ("value" + i).getBytes(StandardCharsets.UTF_8);
-            values.add(new KV.ValueNode(time, type, qualifier, 0, qualifier.length, value, 0, value.length));
-        }
-        KV kv = new KV(row, 0, row.length, family, 0, family.length, values);
-        KeyValueSkipListSet kvs = new KeyValueSkipListSet(new KV.KVComparator());
-        kvs.add(kv);
-        FileStore fileStore=new FileStore(cfMeta,kvs);
+        FileStore fileStore=new FileStore(cfMeta);
         VCFIleWriter.writerAll(fileStore.getData(), fileStoreName);
     }
 }
