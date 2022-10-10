a8b8051d0a09325ca0162f2f7174ce1dd7d5c588
lhh1171 <2807652103@qq.com>
2022-10-10 22:51:34 +0800

    testRegionServer

--- vcdb-RegionServer/src/main/java/org/example/vcdb/store/region/DaemonRegion.java
+++ vcdb-RegionServer/src/main/java/org/example/vcdb/store/region/DaemonRegion.java
@@ -1,5 +1,9 @@
 package org.example.vcdb.store.region;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @ClassName DaemonRegion
  * @Description TODO
@@ -10,6 +14,15 @@ package org.example.vcdb.store.region;
 
 public class DaemonRegion {
     public static void main(String[] args) {
+        int count=5;
+        //一秒检查一下size,count-1
+        //当检查到满的时候调用落盘，或者检查到count==0时候落盘，落盘之后都会把count还原成5
+        // 创建任务队列   10 为线程数量
+        ScheduledExecutorService scheduledExecutorService =
+                Executors.newScheduledThreadPool(10);
+        // 执行任务,里面写执行代码
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
 
+        }, 1, 1, TimeUnit.SECONDS); // 1s 后开始执行，每 3s 执行一次
     }
 }
